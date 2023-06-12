5debd64992ac48759776bcbd71a0a561172ba090
Marat <evolution99@rambler.ru>
2023-06-12 04:18:11 +0300

    Added home page

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,9 +3,51 @@ import "./index.css";
 export const Home = () => {
   return (
     <div className="home">
-      <section>
-        <h1>Home</h1>
+      <section role="navigation" className="main-nav js-main-nav">
+        <a href="#saudi-app" className="main-nav__skip-nav">
+          Skip to content
+        </a>
+        <img
+          src={require("../../images/spacer.gif")}
+          srcSet={`${require("../../images/affiliateLogo.png")},  ${require("../../images/affiliateLogo-2x.png")} 2x`}
+          alt="Aramco Logo"
+        />
+      </section>
+
+    </div>
+  );
+};
+
+
+/*
+export const Home = () => {
+  return (
+    <div className="home">
+      <section role="navigation" className="main-nav js-main-nav">
+        <div className="main-nav__dummy" ref="dummy"></div>
+        <div className="main-nav__float-wrap" ref="floating">
+          <a href="#saudi-app" className="main-nav__skip-nav">
+            Skip to content
+          </a>
+          <div className="main-nav__control-bar"></div>
+          <button
+            className="main-nav__open-btn"
+            aria-controls="main-nav__container--identifier"
+          >
+            Menu
+          </button>
+          <div className="main-nav__logo-small">
+            <a href="/en/">
+              <img
+                src={require("../../images/spacer.gif")}
+                srcSet={`${require("../../images/affiliateLogo.png")},  ${require("../../images/affiliateLogo-2x.png")} 2x`}
+                alt="Aramco Logo"
+              />
+            </a>
+          </div>
+        </div>
       </section>
     </div>
   );
 };
+*/
